3ba308b3c60da87a92cd5cc41b4e1d6f4ff16d1b
ChibaniAdam <adam.chibani@esprit.tn>
2022-11-08 12:13:15 +0100

    json-server

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,64 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  public list: Product[] =  [
-    {
-      id: 12,
-      name: 'T-shirt 1',
-      description :'nice T-shirt',
-      price: 190,
-      nbrLike: 23,
-      quantity: 3,
-      picture:'https://www.exist.com.tn/61575-large_default/t-shirt.jpg',
-      category: 'men'
-    },
-    {
-      id: 13,
-      name: 'T-shirt 2',
-      description :'nice T-shirt',
-      price: 179,
-      nbrLike: 0,
-      quantity: 0,
-      picture:'https://www.exist.com.tn/69177-large_default/t-shirt-de-sport.jpg',
-      category: 'men'
-    },
-    {
-      id: 14,
-      name: 'T-shirt 3',
-      description :'nice T-shirt',
-      price: 250,
-      nbrLike: 0,
-      quantity: 2,
-      picture:'https://cdn.shopify.com/s/files/1/0528/7480/5443/products/tshirt-merci-paris-bleu-front_600x.jpg?v=1653385654',
-      category: 'women'
-    }, {
-      id: 15,
-      name: 'T-shirt 4',
-      description :'nice T-shirt',
-      price: 50,
-      nbrLike: 0,
-      quantity: 10,
-      picture:'https://i.etsystatic.com/13416370/r/il/e476ce/3525121296/il_fullxfull.3525121296_8o3l.jpg',
-      category: 'kids'
-    }
-    
-  ]
-  public delete():void{
-    
+  public url=environment.url+'products/'
+  public list: Product[] =  []
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(){
+    return this.http.get<Product[]>(this.url);
+  }
+  addProduct(p:Product){
+    return this.http.post(this.url,p);
+  }
+  public delete(id: number){
+    return this.http.delete(this.url+id);
   }
   public getById(id:number):any{
     for (let p of this.list) {
       if(p.id==id) return p;
       }
   }
-  public update(p2:Product):void{
-    for (let p of this.list) {
-      if(p.id==p2.id) p=p2 ;
-      }
+  public update(p:Product){
+    return this.http.put(this.url+p.id,p);
   }
-  constructor() { }
 }
